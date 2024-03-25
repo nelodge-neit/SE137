c5125b2f7db8b9ea0b9dae2101dc50ab9c84084e
nathan lodge <42787085+hexiro@users.noreply.github.com>
2024-03-25 14:32:01 -0400

    only show single section at once

---
+++ week1/bio/scripts/index.js
@@ -0,0 +1,30 @@
+// wish i was using typescript :sob:
+
+/**
+ * @type {HTMLAnchorElement[]}
+ */
+const anchors = Array.from(document.querySelectorAll(".nav-anchor"));
+
+/**
+ * @param {MouseEvent} e
+ */
+const onClick = (e) => {
+  e.preventDefault();
+  
+  /**
+   * @type {HTMLAnchorElement}
+   */
+  const anchor = e.currentTarget;
+  const index = anchors.indexOf(anchor);
+  const sections = document.querySelectorAll("section");
+
+  for (const section of sections) {
+    section.classList.add("hide");
+  }
+
+  sections[index].classList.remove("hide");
+};
+
+for (const anchor of anchors) {
+  anchor.addEventListener("click", onClick);
+}
