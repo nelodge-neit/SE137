1e018955634e664100f0ecd184141f183a8dde7a
nathan lodge <42787085+hexiro@users.noreply.github.com>
2024-04-29 14:29:29 -0400

    keep text saved when switching tab

--- week6/vsc/scripts/index.js
+++ week6/vsc/scripts/index.js
@@ -5,16 +5,24 @@ const breadcrumbsLinks = Array.from(
   document.querySelectorAll("#breadcrumbs a")
 );
 
+const textArea = document.querySelector("textarea");
+const textAreaContentList = new Array(tabs.length).fill("");
+
 for (const tab of tabs) {
   const onClick = (e) => {
+    const oldIndex = tabs.indexOf(document.querySelector(".tab-button.active"));
+
     for (const t of tabs) {
       t.classList.remove("active");
     }
-
+    // set the clicked tab to active
     const target = e.currentTarget;
     target.classList.add("active");
-
+    // set the breadcrumbs to the clicked tab
     breadcrumbsLinks.at(-1).textContent = target.textContent;
+    // set the textarea content to the value stored in the clicked tab (virtual file)
+    textAreaContentList[oldIndex] = textArea.value;
+    textArea.value = textAreaContentList[tabs.indexOf(target)];
   };
 
   tab.addEventListener("click", onClick);
@@ -22,9 +30,8 @@ for (const tab of tabs) {
 
 // OPEN TRAY
 
+const tray = document.querySelector("#tray");
 const trayButton = document.querySelector("#tray-button");
 trayButton.addEventListener("click", () => {
-  console.log("click!");
-  const tray = document.querySelector("#tray");
   tray.classList.toggle("hide");
 });
